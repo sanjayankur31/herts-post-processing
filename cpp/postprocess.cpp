3a8a994af0bb8da38bb192a8521502b041b1add6
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2015-01-30 20:02:04 +0000

    Intermediate code commit.

--- cpp/postprocess.cpp
+++ cpp/postprocess.cpp
@@ -82,27 +82,88 @@ calculateTimeToPlotList (int num_pat, std::vector <unsigned int> stage_times)
 }		/* -----  end of function calculateTimeToPlotList  ----- */
 
 
-
 /* 
  * ===  FUNCTION  ======================================================================
- *         Name:  timeLords
+ *         Name:  tardis
  *  Description:  
  * =====================================================================================
  */
     void
-timeLords (std::multimap<double, unsigned int> &dataE, std::multimap<double, unsigned int> &dataI, double timeToFly, std::string output_file  )
+tardis (std::multimap<double, unsigned int> &dataE, std::multimap<double, unsigned int> &dataI, std::vector<std::vector<unsigned int> >& patterns, std::vector<std::vector <unsigned int> >& recalls, double timeToFly, struct param parameters)
 {
     std::vector <unsigned int>neuronsE;
     std::vector <unsigned int>neuronsI;
-    std::cout << "Thread " << std::this_thread::get_id() << " working on it, Sir!" << "\n"
+    std::vector <std::vector<unsigned int> >pattern_neurons_rate;
+    std::vector <std::vector<unsigned int> >noise_neurons_rate;
+    std::vector <std::vector<unsigned int> >recall_neurons_rate;
+    std::vector <unsigned int>neuronsE_rate;
+    std::vector <unsigned int>neuronsI_rate;
+
+    converter.str("");
+    converter.clear();
+    converter << parameters.patternfile_prefix <<  std::setw(8)  << std::setfill('0') << std::to_string(i) << ".pat";
+    /*  Throw into parameter file! */
+    int rowWidthE = 80;
+    int rowWidthI = 20;
+
+    std::cout << "[TARDIS] Thread " << std::this_thread::get_id() << " working on it, Sir!" << "\n";
     for(std::multimap<double, unsigned int>::iterator it = dataI.lower_bound(timeToFly -1.); it != dataI.upper_bound(timeToFly); ++it)
-        neuronsI.emplace_back(it->second)
+        neuronsI.emplace_back(it->second);
+
+    for(std::vector<unsigned int>::const_iterator it = neuronsI.cbegin(); it != neuronsI.cend(); ++it)
+    {
+
+    }
+
+
+
 
     for(std::multimap<double, unsigned int>::iterator it = dataE.lower_bound(timeToFly -1.); it != dataE.upper_bound(timeToFly); ++it)
-        neuronsE.emplace_back(it->second)
+        neuronsE.emplace_back(it->second);
+    std::sort(neuronsE.begin(), neuronsE.end());
 
-    std::cout << "Thread " << std::this_thread::get_id() << " reporting on conclusion, Sir!" << "\n"
-}		/* -----  end of function timeLords  ----- */
+    std::vector<unsigned int>::iterator search_begin = neuronsE.begin();
+    for(unsigned int i = 1; i <= parameters.NE; ++i)
+    {
+        int rate = 0;
+        rate = (std::upper_bound(search_begin, neuronsE.end(), i) != neuronsE.end()) ?  (std::upper_bound(search_begin, neuronsE.end(), i) - search_begin) : 0;
+        search_begin = std::upper_bound(search_begin, neuronsE.end(), i);
+
+        for (unsigned int j = 0; j < parameters.num_pats; j++)
+        {
+            if(pattern_neurons_rate[j].size() != patterns[j].size() && std::binary_search(patterns[j].begin(), patterns[j].end(), i))
+            {
+                pattern_neurons_rate[j].emplace_back(rate);
+            }
+            else
+            {
+                noise_neurons_rate[j].emplace_back(rate);
+            }
+            if(recall_neurons_rate[j].size() != recalls[j].size() && std::binary_search(recalls[j].begin(), recalls[j].end(), i))
+            {
+                recall_neurons_rate[j].emplace_back(rate);
+            }
+        }
+        neuronsE_rate.emplace_back(rate);
+    }
+
+    for(unsigned int i = 0; i < parameters.NE; ++i)
+    {
+        std::ofstream neuronsE_stream;
+        std::ofstream neuronsI_stream;
+        std::ofstream pattern_neurons_stream;
+        std::ofstream recall_neurons_stream;
+
+        for (unsigned int j = 0; j < parameters.num_pats; j++)
+
+
+    }
+
+
+
+
+    std::cout << "[TARDIS] Thread " << std::this_thread::get_id() << " reporting on conclusion, Sir!" << "\n";
+}		/* -----  end of function tardis  ----- */
 
 /* 
  * ===  FUNCTION  ======================================================================
@@ -127,6 +188,49 @@ populateMaps(std::multimap<double, unsigned int> &map_to_populate, std::string f
 }
 
 
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  getPatternsAndRecalls
+ *  Description:  
+ * =====================================================================================
+ */
+    void
+getPatternsAndRecalls ( std::vector<std::vector<unsigned int> > &patterns, std::vector<std::vector<unsigned int> >&recalls, struct param parameters)
+{
+    clock_t clock_start = clock();
+    std::ostringstream converter;
+    unsigned int neuronID;
+    std::ifstream ifs;
+    for (unsigned int i = 1; i <= parameters.num_pats; ++i)
+    {
+        converter.str("");
+        converter.clear();
+        converter << parameters.patternfile_prefix <<  std::setw(8)  << std::setfill('0') << std::to_string(i) << ".pat";
+        std::string pattern = converter.str();
+        ifs.open(pattern);
+        while(ifs >> neuronID)
+            patterns[i].emplace_back(neuronID);
+        ifs.close();
+        std::sort(patterns[i].begin(), patterns[i].end());
+        std::cout << "Items loaded and sorted in pattern number " << i << " is " << patterns[i].size() << "\n";
+
+        converter.str("");
+        converter.clear();
+        converter << parameters.recallfile_prefix <<  std::setw(8)  << std::setfill('0') << std::to_string(i) << ".pat";
+        std::string recall = converter.str();
+        ifs.open(recall);
+        while(ifs >> neuronID)
+            recalls[i].emplace_back(neuronID);
+        ifs.close();
+        std::sort(recalls[i].begin(), recalls[i].end());
+        std::cout << "Items loaded and sorted in recall number " << i << " is " << recalls[i].size() << "\n";
+    }
+    clock_t clock_end = clock();
+    std::cout << "Patterns and recalls read in " << (clock_end - clock_start)/CLOCKS_PER_SEC << " seconds.\n";
+
+}		/* -----  end of function getPatternsAndRecalls  ----- */
+
 /* 
  * ===  FUNCTION  ======================================================================
  *         Name:  main
@@ -140,10 +244,10 @@ main(int ac, char* av[])
     clock_t clock_start, clock_end;
     std::multimap<double, unsigned int> raster_data_E;
     std::multimap<double, unsigned int> raster_data_I;
-    std::vector<std::thread> thread_vector;
-    int threads_max = 12;
-
-
+    std::vector<std::thread> timeLords;
+    int doctors_max = 12;
+    std::vector<std::vector<unsigned int> > patterns;
+    std::vector<std::vector<unsigned int> > recalls;
 
     try {
 
@@ -203,31 +307,34 @@ main(int ac, char* av[])
 
     if (parameters.excitatory_raster_file != "")
     {
-        thread_vector.emplace_back(std::thread (populateMaps, std::ref(raster_data_E), parameters.excitatory_raster_file));
+        timeLords.emplace_back(std::thread (populateMaps, std::ref(raster_data_E), parameters.excitatory_raster_file));
     } 
-    /*  Use the main thread! */
     if (parameters.inhibitory_raster_file != "")
     {
-        thread_vector.emplace_back(std::thread (populateMaps, std::ref(raster_data_I), parameters.inhibitory_raster_file));
+        timeLords.emplace_back(std::thread (populateMaps, std::ref(raster_data_I), parameters.inhibitory_raster_file));
     }
 
-    for (std::thread &t: thread_vector)
+    /*  Use the main thread! */
+    getPatternsAndRecalls(std::ref(patterns), std::ref(recalls), parameters);
+
+    for (std::thread &t: timeLords)
     {
         if(t.joinable())
         {
             t.join();
         }
     }
-    thread_vector.clear();
+    timeLords.clear();
     /*  I need to wait for files to be read before I do anything else */
 
     clock_start = clock();
-    for(int task_counter = 0, std::vector<double>::const_iterator i = graphing_times.begin(); i != graphing_times.end(); ++i)
+    int task_counter = 0;
+    for(std::vector<double>::const_iterator i = graphing_times.begin(); i != graphing_times.end(); ++i)
     {
         /*  Only start a new thread if less than thread_max threads are running */
-        if (task_counter < threads_max)
+        if (task_counter < doctors_max)
         {
-            thread_vector.emplace_back(std::thread (timeLords, std::ref(raster_data_E), std::ref(raster_data_I), *i, parameters.output_file));
+            timeLords.emplace_back(std::thread (tardis, std::ref(raster_data_E), std::ref(raster_data_I), std::ref(patterns), std::ref(recalls), *i, parameters));
             task_counter++;
         }
         /*  If thread_max threads are running, wait for them to finish before
@@ -239,7 +346,7 @@ main(int ac, char* av[])
          */
         else
         {
-            for (std::thread &t: thread_vector)
+            for (std::thread &t: timeLords)
             {
                 if(t.joinable())
                 {
@@ -247,19 +354,19 @@ main(int ac, char* av[])
                     task_counter--;
                 }
             }
-            thread_vector.clear();
+            timeLords.clear();
         }
     }
 
     /*  Wait for remaining threads to finish */
-    for (std::thread &t: thread_vector)
+    for (std::thread &t: timeLords)
     {
         if(t.joinable())
         {
             t.join();
         }
     }
-    thread_vector.clear();
+    timeLords.clear();
     clock_end = clock();
     std::cout << "Time taken for the processing part is: " << (clock_end - clock_start)/CLOCKS_PER_SEC << "\n";
     return 0;
