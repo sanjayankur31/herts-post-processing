ea5e0116ff77ddfaa1e33e0c1be6f286649ee88a
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2015-02-25 12:13:05 +0000

    Rename the conf file in the output directory.

--- scripts/run_vogels_mpich.sh
+++ scripts/run_vogels_mpich.sh
@@ -42,7 +42,7 @@ function default()
     pushd "$newsimenv"
         cp "$configfile" ./$newsimenv.cfg -v
         echo "$ mpiexec -n $mpi_ranks vogels --out $newsimenv"
-        LD_LIBRARY_PATH=/home/asinha/Documents/02_Code/00_repos/00_mine/herts-research-repo/auryn/src/.libs mpiexec -n $mpi_ranks ~/bin/research-bin/vogels --out $newsimenv
+        LD_LIBRARY_PATH=/home/asinha/Documents/02_Code/00_repos/00_mine/herts-research-repo/auryn/src/.libs mpiexec -n $mpi_ranks ~/bin/research-bin/vogels --out $newsimenv --config $newsimenv".cfg"
     popd
     echo "Result directory is: $newsimenv"
     tmux set-buffer "$newsimenv"
