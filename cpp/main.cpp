a520d61efae82b55345d02cc029bf5e796251cc8
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2015-02-25 18:07:55 +0000

    Initial commit with new files. Added one method.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -18,14 +18,84 @@
 
 #include "utils.h"
 
+/*  Holds my parameters both from the configuration file and the command line
+ *  Since this is global, I'm not passing parameters as an argument to any
+ *  function 
+ */
+struct param parameters;
+
 /* 
  * ===  FUNCTION  ======================================================================
  *         Name:  main
- *  Description:  
+ *  Description:  Main method with post processing logic
  * =====================================================================================
  */
     int
 main ( int argc, char *argv[] )
 {
+    clock_t global_clock_start, global_clock_end;
+    global_clock_start = clock();
+
+    try {
+
+        po::options_description cli("Command line options");
+        cli.add_options()
+            ("help,h", "produce help message")
+            ("out,o", po::value<std::string>(&(parameters.output_file)), "output filename")
+            ("config,c", po::value<std::string>(&(parameters.config_file))->default_value("simulation_config.cfg"),"configuration file to be used to find parameter values for this simulation run")
+            ;
+
+
+        po::options_description params("Parameters");
+        params.add_options()
+            ("patternfile_prefix", po::value<std::string>(&(parameters.patternfile_prefix)), "Pattern files prefix")
+            ("recallfile_prefix", po::value<std::string>(&(parameters.recallfile_prefix)), "Recall files prefix")
+            ("num_pats", po::value<unsigned int>(&(parameters.num_pats)), "number of pattern file(s) to pick from pattern set")
+            ("graph_pattern_cols", po::value<unsigned int>(&(parameters.graph_pattern_cols)), "Columns in the per pattern matrices - multiple of 10 please")
+            ("NE", po::value<unsigned int>(&(parameters.NE)), "Number of excitatory neurons")
+            ("graph_widthE", po::value<unsigned int>(&(parameters.graph_widthE)), "Excitatory columns in the final matrix")
+            ("NI", po::value<unsigned int>(&(parameters.NI)), "Number of inhibitory neurons")
+            ("graph_widthI", po::value<unsigned int>(&(parameters.graph_widthI)), "Inhibitory columns in the final matrix")
+            ("stage_times", po::value<std::vector<unsigned int> >(&(parameters.stage_times))-> multitoken(), "comma separated list of times for each stage in order")
+            ("plot_times", po::value<std::vector<double> >(&(parameters.plot_times))-> multitoken(), "comma separated list of additional plot times")
+            ("mpi_ranks", po::value<unsigned int>(&(parameters.mpi_ranks)), "Number of MPI ranks being used")
+            ;
+
+        po::options_description visible("Allowed options");
+        visible.add(cli).add(params);
+
+        po::variables_map vm;
+        po::store(po::basic_command_line_parser<char>(ac, av).options(cli).allow_unregistered().run(), vm);
+        po::notify(vm);
+
+        if (vm.count("help")) {
+            std::cout << visible << "\n";
+            return 0;
+        }
+
+        std::ifstream ifs(parameters.config_file.c_str());
+        if (!ifs)
+        {
+            std::cout << "Cannot open configuration file - " << parameters.config_file << "\n";
+            return -1;
+        }
+        else
+        {
+            po::store(po::parse_config_file(ifs, params, true), vm);
+            po::notify(vm);
+        }
+    }
+    catch(std::exception &e) {
+        std::cerr << "error: " << e.what() << "\n";
+        return 1;
+    }
+    catch(...) {
+        std::cerr << "Exception of unknown type!\n";
+    }
+
+    /*  Load patterns and recalls */
+    LoadPatternsAndRecalls(std::ref(patterns), std::ref(recalls), parameters);
+    global_clock_end = clock();
+    std::cout << "Total time taken: " << (global_clock_end - global_clock_start)/CLOCKS_PER_SEC << "\n";
     return 0;
 }				/* ----------  end of function main  ---------- */
--- cpp/utils.h
+++ cpp/utils.h
@@ -35,6 +35,10 @@
 #include "gnuplot-iostream/gnuplot-iostream.h"
 
 
+/*-----------------------------------------------------------------------------
+ *  DECLARATIONS
+ *-----------------------------------------------------------------------------*/
+
 namespace po = boost::program_options;
 
 /*  Make sure this matches in auryn, or you break down */
@@ -82,7 +86,90 @@ struct param
 
     unsigned int mpi_ranks;
 
-} parameters;
+};
 
+
+/*-----------------------------------------------------------------------------
+ *  FUNCTIONS
+ *-----------------------------------------------------------------------------*/
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  LoadPatternsAndRecalls
+ *  Description:  Loads patterns and recalls from files into given vectors
+ *
+ *  Returns: void
+ *  Arguments:
+ *      - patterns: reference to 2D vector that will hold my patterns
+ *      - recalls: reference to 2D vector that will hold my recalls
+ * =====================================================================================
+ */
+    void
+LoadPatternsAndRecalls ( std::vector<std::vector<unsigned int> > &patterns, std::vector<std::vector<unsigned int> >&recalls)
+{
+#if  DEBUG_ENABLED
+    clock_t clock_start, clock_end;
+    clock_start  = clock();
+#endif     /* -----  DEBUG_ENABLED  ----- */
+    std::ostringstream converter;
+    unsigned int neuronID;
+    std::ifstream ifs;
+
+    for (unsigned int i = 0; i < parameters.num_pats; ++i)
+    {
+        converter.str("");
+        converter.clear();
+        converter << parameters.patternfile_prefix <<  std::setw(8)  << std::setfill('0') << std::to_string(i+1) << ".pat";
+        std::string pattern = converter.str();
+        patterns.emplace_back(std::vector<unsigned int> ());
+
+        try 
+        {
+            ifs.open(pattern);
+
+            while(ifs >> neuronID)
+                patterns[i].emplace_back(neuronID);
+
+            ifs.close();
+            std::sort(patterns[i].begin(), patterns[i].end());
+#if  DEBUG_ENABLED
+            std::cout << "Items loaded and sorted in pattern number " << i << " is " << patterns[i].size() << "\n";
+#endif     /* -----  DEBUG_ENABLED  ----- */
+        }
+        catch (std::ios_base::failure &e)
+        {
+            std::cerr << e.what() << "\n";
+        }
+
+        converter.str("");
+        converter.clear();
+        converter << parameters.recallfile_prefix <<  std::setw(8)  << std::setfill('0') << std::to_string(i+1) << ".pat";
+        std::string recall = converter.str();
+        recalls.emplace_back(std::vector<unsigned int> ());
+
+        try 
+        {
+            ifs.open(recall);
+
+            while(ifs >> neuronID)
+                recalls[i].emplace_back(neuronID);
+
+            ifs.close();
+            std::sort(recalls[i].begin(), recalls[i].end());
+#if  DEBUG_ENABLED
+            std::cout << "Items loaded and sorted in recall number " << i << " is " << recalls[i].size() << "\n";
+#endif     /* -----  DEBUG_ENABLED  ----- */
+        }
+        catch (std::ios_base::failure &e)
+        {
+            std::cerr << e.what() << "\n";
+        }
+    }
+#if  DEBUG_ENABLED
+    clock_end = clock();
+    std::cout << patterns.size() << " patterns and " << recalls.size() << " recalls read in " << (clock_end - clock_start)/CLOCKS_PER_SEC << " seconds.\n";
+
+#endif     /* -----  DEBUG_ENABLED  ----- */
+}		/* -----  end of function LoadPatternsAndRecalls  ----- */
 
 #endif   /* ----- #ifndef utils_INC  ----- */
