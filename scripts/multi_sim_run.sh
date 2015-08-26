a685d6bd6189d84f0f3d1bf3b2857f1f74babf67
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2015-08-26 12:50:01 +0100

    for presentations.

--- scripts/multi_sim_run.sh
+++ scripts/multi_sim_run.sh
@@ -30,7 +30,7 @@ do
     ~/bin/research-scripts/run_vogels_mpich.sh
     ~/bin/research-scripts/postprocess-mpich.sh -d `tmux show-buffer` -o
     pushd `tmux show-buffer`
-        ~/bin/research-bin/postprocess -o `tmux show-buffer` -c `tmux show-buffer`.cfg -S -s && rm *.ras *.netstate *.weightinfo *.rate *.log *merge* -f
+        ~/bin/research-bin/postprocess -o `tmux show-buffer` -c `tmux show-buffer`.cfg -S -s -e && rm *.ras *.netstate *.weightinfo *.rate *.log *merge* -f
     popd
     echo `tmux show-buffer` >> dirs-created.txt
     rm -rf ~/dump/patternFilesTemp/ && sleep 1
