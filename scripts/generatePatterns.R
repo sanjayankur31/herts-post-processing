b61119b40ae493a4d66bd4e3f30154ee0d2e6482
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2015-11-03 12:47:58 +0000

    Need to update generation script to take arguments.

--- scripts/generatePatterns.R
+++ scripts/generatePatterns.R
@@ -5,8 +5,8 @@
 NE <- 8000
 NI <- 2000
 
-sparsityE <- 0.1
-recallsize <- 0.25
+patternPercentOfTotal <- 0.1
+recallPercentOfPattern <- 0.50
 
 numberOfRandomPatternFiles <- 60
 patternFilePrefix <- "randomPatternFile-"
@@ -16,8 +16,8 @@ for (filenumber in c(1:numberOfRandomPatternFiles))
 {
     patternFileName <- sprintf("%s%08d%s",patternFilePrefix,filenumber,".pat")
     recallFileName <- sprintf("%s%08d%s",recallFilePrefix,filenumber,".pat")
-    randomlist <- sample(0:NE,NE*sparsityE,replace=F)
-    recalllist <- sample(randomlist, NE*sparsityE*recallsize,replace=F)
+    randomlist <- sample(0:NE,NE*patternPercentOfTotal,replace=F)
+    recalllist <- sample(randomlist, NE*patternPercentOfTotal*recallPercentOfPattern,replace=F)
 
     writeLines(as.character(randomlist),patternFileName,sep="\n")
     writeLines(as.character(recalllist),recallFileName,sep="\n")
--- scripts/multi_sim_run.sh
+++ scripts/multi_sim_run.sh
@@ -22,16 +22,19 @@
 runs="$1"
 for i in `seq 1 $runs`:
 do
-    mkdir ~/dump/patternFilesTemp/
-    pushd ~/dump/patternFilesTemp/
-        Rscript ~/bin/research-scripts/generatePatterns.R
-    popd
+    #mkdir ~/dump/patternFilesTemp/
+ #   pushd ~/dump/patternFilesTemp/
+  #      Rscript ~/bin/research-scripts/generatePatterns.R
+   # popd
     # make sure the configuration file is up to date
     ~/bin/research-scripts/run_vogels_mpich.sh
     ~/bin/research-scripts/postprocess-mpich.sh -d `tmux show-buffer` -o
     pushd `tmux show-buffer`
-        #~/bin/research-bin/postprocess -o `tmux show-buffer` -c `tmux show-buffer`.cfg -S -s -e --pattern && rm *.ras *.netstate *.weightinfo *.rate *.log *merge* *bras -f
-        ~/bin/research-bin/postprocess -o `tmux show-buffer` -c `tmux show-buffer`.cfg -S -s -e --pattern
+        sed -i '/^%/ d' 00-Con_* # get rid of useless headers
+        sort -g -m 00-Con_ee* > 00-Con_ee.txt
+        sort -g -m 00-Con_ie* > 00-Con_ie.txt
+        ~/bin/research-bin/postprocess -o `tmux show-buffer` -c `tmux show-buffer`.cfg -S -s -e --pattern && rm *.ras *.netstate *.weightinfo *.rate *.log *merge* *bras -f
+        #~/bin/research-bin/postprocess -o `tmux show-buffer` -c `tmux show-buffer`.cfg -S -s -e --pattern
         sed '/^%/ d ' 00-Con_ee.txt |  cut -f2 -d " " |sort -n | uniq -c > 00-uniq-ee.txt
         sed '/^%/ d ' 00-Con_ie.txt |  cut -f2 -d " " |sort -n | uniq -c > 00-uniq-ie.txt
         sed '/^%/ d ' 00-Con_ee.txt |  cut -f2 -d " " > 00-all-ee.txt
@@ -39,7 +42,7 @@ do
         wc -l 00-uniq* 00-all* > 00-conn-stats.txt
     popd
     echo `tmux show-buffer` >> dirs-created.txt
-    rm -rf ~/dump/patternFilesTemp/ && sleep 1
+    #rm -rf ~/dump/patternFilesTemp/ && sleep 1
 done
 
 echo "$runs simulations finished. Post process away!"
